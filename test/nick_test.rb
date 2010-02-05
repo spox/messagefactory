e30f59557f5ddf96c0213631fa513cf1f3fe3f60
spox <spox@modspox.com>
2010-02-05 13:08:51 -0800

    add nick handler and test

---
+++ test/nick_test.rb
@@ -0,0 +1,33 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+$:.unshift File.join(File.dirname(__FILE__),'..','lib')
+
+require 'test/unit'
+require 'Handler'
+require 'handlers/Nick'
+
+class NickTest < Test::Unit::TestCase
+    def setup
+        @handler = MessageFactory::Handlers::Nick.new
+    end
+    def test_types
+        assert_equal(@handler.types_process, :NICK)
+    end
+    def test_good
+        string = ':spox!~spox@some.random.host NICK :flock_of_deer'
+        m = @handler.process(string)
+        assert_kind_of(OpenStruct, m)
+        assert_kind_of(Time, m.received)
+        assert_equal(:incoming, m.direction)
+        assert_equal(:nick, m.type)
+        assert_equal(string, m.raw)
+        assert_equal('spox!~spox@some.random.host', m.source)
+        assert_equal('flock_of_deer', m.new_nick)
+    end
+    def test_bad
+        assert_raise(RuntimeError) do
+            @handler.process('some bad string')
+        end
+    end
+end
